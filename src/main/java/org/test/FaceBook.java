b865131aa47f4ca7ca6751348f878a78b382950e
HariharanMathiyalagan <hariharan.m@zaigoinfotech.com>
2023-06-14 23:44:31 +0530

    Checnged

--- src/main/java/org/test/FaceBook.java
+++ src/main/java/org/test/FaceBook.java
@@ -3,21 +3,36 @@ package org.test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FaceBook {
+	WebDriver driver;
 
-	public static void main(String[] args) {
+	@BeforeClass
+	public void beforeLaunch() {
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver = new ChromeDriver();
+		driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get("https://www.facebook.com/login");
+
+	}
+
+	@AfterClass
+	private void afterLaunch() {
+		driver.quit();
+	}
+
+	@Test
+	public void test1() {
 		driver.findElement(By.id("email")).sendKeys("978456231");
 		driver.findElement(By.id("pass")).sendKeys("Hari@123");
 		driver.findElement(By.id("loginbutton")).click();
 		System.out.println(driver.findElement(By.xpath("//*[text()='Wrong credentials']")).getText());
-		driver.quit();
+
 	}
 
 }
